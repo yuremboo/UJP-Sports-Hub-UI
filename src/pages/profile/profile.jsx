745a860c05d61618fb1fe2c90083ee08d9189628
pro100proger <nazar4075066@gmail.com>
2022-09-28 18:09:47 +0300

    some fixes

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -8,12 +8,28 @@ import ChangePassword from "../../Components/ChangePassword/ChangePassword";
 import SubscriptionUser from "../../Components/Subscriptions/SubscriptionUser";
 import {useEffect} from "react";
 import {render} from "react-dom";
+import accountSwitcher from "../../icons/accountSwitcher.svg";
+import ProfileSection from "../../Components/profileSectionHeader/profileSection";
+import SaveCancelChanges from "../../Components/SaveCancelChanges/SaveCancelChanges";
+import HorizontalScrollMenu from "../../Components/horizontal-scroll-menu/horizontalScrollMenu";
 
 const Profile = ({props, globalStore}) => {
     const {profile} = useParams();
         return (
             <div className={"update-user-information"}>
-                <Header/>
+                    <div className="profile_admin__header__outer_fixed">
+                        <div className="profile_admin__header">
+                            <div className="sportshub">Sports hub</div>
+                            <div className="n_all_articles_admin__right_header">
+                                <button className="n_accountSwitcher__button">
+                                    <img src={accountSwitcher} width="30%" height="30%"/>
+                                </button>
+                                <div className="n_admin__profile_section">
+                                    <ProfileSection/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                 <div className={"update-user__wrapper"}>
                     <div className={"nav-bar-user"}>
